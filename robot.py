60d4e758c338a0fdadfabc322a3b1b3d58ae5bb3
Umut Erçin Baş <umutercinbas@gmail.com>
2023-05-15 00:23:42 +0300

    add robot module

---
+++ robot.py
@@ -0,0 +1,33 @@
+from typing import List
+
+import time
+
+from ultrasonic_sensor import DummyUltrasonicSensor, random_data_generator
+
+class Robot:
+    distance_threshold = 10
+    average_distance: float = 0
+
+    def navigate_robot(self):
+        if self.average_distance < self.distance_threshold:
+            return" Stop"
+        else:
+            return "Continue"
+
+    def start(self):
+        distance_list = []
+
+        distance_sensor = DummyUltrasonicSensor(random_data_generator(0, 18))
+
+        time_checkpoint = time.time()
+        while True:
+            if time.time() - time_checkpoint >= 10:
+                current_distance = distance_sensor.get_distance()
+
+                distance_list = [*distance_list[1:21], current_distance]
+
+                time_checkpoint = time.time()
+            
+            self.average_distance = sum(distance_list) / len(distance_list)
+            
+            print(self.navigate_robot())
